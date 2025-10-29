5dbc487e2ed975662d446abbe071e80d2ecd1a33
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2025-10-29 21:25:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # ────────────────────────────────────────────────────────────────────────────
 # 기본 모듈,라이브러리 로드
 # ────────────────────────────────────────────────────────────────────────────
-import asyncio, io, httpx, discord, random, re, datetime, logging, os, certifi, ssl, itertools, string, time, json                            
+import asyncio, io, httpx, discord, random, re, datetime, logging, os, certifi, ssl, itertools, string, time, json                             
 from discord.ext import commands
 from pytz import timezone
 from typing import Optional, List
